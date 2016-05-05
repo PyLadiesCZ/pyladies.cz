7cc8a7beb003924d7f5b6ba3649e7c46a44e2c6d
Petr Viktorin <encukou@gmail.com>
2016-05-05 09:29:24 +0200

    Build static site with Frozen-Flask

--- pyladies_cz.py
+++ pyladies_cz.py
@@ -1,16 +1,26 @@
+"""Create or serve the pyladies.cz website
+"""
+
 import sys
-import os
-
-from flask import Flask, render_template, url_for, send_from_directory
-import yaml
-import jinja2
-import markdown
-
 if sys.version_info < (3, 0):
     raise RuntimeError('You need Python 3.')
 
+import os
+import fnmatch
+import urllib.parse
+
+from flask import Flask, render_template, url_for, send_from_directory
+from flask import redirect
+from flask_frozen import Freezer
+import yaml
+import jinja2
+import markdown
+import click
+
 app = Flask('pyladies_cz')
 
+orig_path = os.path.join(app.root_path, 'original/')
+v1_path = os.path.join(orig_path, 'v1/')
 
 ########
 ## Views
@@ -20,29 +30,41 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/brno_info')
+@app.route('/brno_info/')
 def brno_info():
     return render_template('brno_info.html')
 
 
-@app.route('/praha_info')
+@app.route('/praha_info/')
 def praha_info():
     return render_template('praha_info.html')
 
 
-@app.route('/brno')
+@app.route('/brno/')
 def brno():
     return render_template('brno.html', plan=read_yaml('plans/brno.yml'))
 
 
-@app.route('/praha')
+@app.route('/praha/')
 def praha():
     return render_template('praha.html', plan=read_yaml('plans/praha.yml'))
 
 
 @app.route('/v1/<path:path>')
 def v1(path):
-    return send_from_directory('original/v1/', path)
+    return send_from_directory(v1_path, path)
+
+@app.route('/index.html')
+def index_html():
+    return redirect(url_for('index'))
+
+@app.route('/course.html')
+def course_html():
+    return send_from_directory(orig_path, 'course.html')
+
+@app.route('/CNAME')
+def cname():
+    return app.config['SERVER_NAME']
 
 
 ##########
@@ -79,6 +101,9 @@ def pathto(name, static=False):
         prefix = '_static/'
         if name.startswith(prefix):
             return url_for('static', filename=name[len(prefix):])
+        prefix = 'v1/'
+        if name.startswith(prefix):
+            return url_for('v1', path=name[len(prefix):])
         return name
     return url_for(name)
 
@@ -90,9 +115,65 @@ def inject_context():
     }
 
 
+##########
+## Freezer
+
+freezer = Freezer(app)
+
+@freezer.register_generator
+def v1():
+    IGNORE = ['*.aux', '*.out', '*.log', '*.scss', '.travis.yml', '.gitignore']
+    for name, dirs, files in os.walk(v1_path):
+        if '.git' in dirs:
+            dirs.remove('.git')
+        for file in files:
+            if not any(fnmatch.fnmatch(file, ig) for ig in IGNORE):
+                path = os.path.relpath(os.path.join(name, file), v1_path)
+                yield {'path': path}
+
+
+def freeze_app(path, base_url):
+    app.config['FREEZER_DESTINATION'] = path
+    app.config['FREEZER_BASE_URL'] = base_url
+    app.config['SERVER_NAME'] = urllib.parse.urlparse(base_url).netloc
+    freezer.freeze()
+
+
 #########
 ## Runner
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8003))
+
+def main():
+    return cli(obj={})
+
+
+@click.group(context_settings=dict(help_option_names=['-h', '--help']),
+             help=__doc__)
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--port', type=int, default=8003,
+              help='Port to listen at')
+def serve(port):
+    """Run a debug server"""
     app.run(host='0.0.0.0', port=port, debug=True)
+
+@cli.command()
+@click.option('--path', default=os.path.join(app.root_path, '_build'),
+              help='Output path')
+@click.option('--base-url', default='http://pyladies.cz/',
+              help='URL for the application, used for external links')
+@click.option('--serve/--no-serve',
+              help='After building the site, run a server with it')
+@click.option('--port', default=8003,
+              help='Port used for --serve')
+def freeze(path, base_url, serve, port):
+    """Build a static site"""
+    freeze_app(path, base_url)
+    if serve:
+        freezer.serve(port=port)
+
+if __name__ == '__main__':
+    cli()
